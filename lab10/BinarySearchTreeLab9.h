29d820fb7c0ac64342e71e601d290e554647b42e
008320644 <008320644@csusb.edu@jb358-7.csusb.edu>
2024-03-27 13:48:31 -0700

    Remove working, print erroring out after for some reason

---
+++ lab10/BinarySearchTreeLab9.h
@@ -0,0 +1,219 @@
+// PARTIAL implementation of BinarySearchTree
+// insert and printing only
+// version for Lab9, 2024 ... to be completed
+
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+#include <cassert>
+#include "Vector.h"
+using namespace std;
+
+
+template <typename T>
+class BinarySearchTree
+{
+private:
+    struct BinaryNode
+    {
+        T element;
+        BinaryNode* left;
+        BinaryNode* right;
+
+        BinaryNode(const T& theElement, BinaryNode* lt, BinaryNode* rt)
+            : element(theElement), left(lt), right(rt)
+        {}
+
+        BinaryNode(const T&& theElement, BinaryNode* lt, BinaryNode* rt)
+            : element(std::move(theElement)), left(lt), right(rt)
+        {}
+    };
+
+public:
+
+    BinarySearchTree() : root(0) {}
+
+    ~BinarySearchTree()
+    {
+        makeEmpty();
+    }
+
+    bool isEmpty() const
+    {
+        return root == 0;
+    }
+    const T & findMin(){
+      assert(!isEmpty());
+      return findMin(root)->element;
+    }
+    void insert(const T& x)
+    {
+        // calls private insert ...
+        insert(x, root);     // ==> LAB 10: add private fct from Module 6.2
+    }
+
+    // LAB 10: print all elements (values) in line
+
+    void printElements() const
+    {
+        if (isEmpty())
+            cout << "Empty tree" << endl;
+        else
+            printElements(root);  // => LAB 10: define private fct below
+    }
+
+    // LAB 10: returns number of elements in BST
+
+    int size() const
+    {
+        return size(root);  // LAB 10: define private fct below
+    }
+
+    void printInternal() const
+    {
+        if (isEmpty())
+            cout << "Empty tree" << endl;
+        else
+            printInternal(root, 0);
+    }
+
+    // for destructor to call...
+    void makeEmpty()
+    {
+        makeEmpty(root);
+    }
+    void remove(const T & x){
+      remove(x, root);
+    }
+private:
+    int big =0;
+    BinaryNode* root;
+
+
+    //the private member fcts that do all the work...
+
+    void insert(const T& x, BinaryNode*& t)
+    {
+        if(t==nullptr){
+          t= new BinaryNode{x, nullptr, nullptr};
+        }
+        else if(t->element > x){
+          return insert(x, t->right);
+        }
+        else if(t->element < x){
+          return insert(x, t->left);
+        }
+    }
+    BinaryNode * findMin(BinaryNode *t) const{
+      if(t==0){
+        return 0;
+      }
+      if(t->left==0){
+        return t;
+      }
+      return findMin(t->left);
+    }
+    void makeEmpty(BinaryNode*& t)
+    {
+        if (t != 0)
+        {
+            makeEmpty(t->left);
+            makeEmpty(t->right);
+            delete t;
+        }
+        t = 0;
+    }
+    void remove(const T& x, BinaryNode*& t){
+      cout<<"1";
+
+      if(t==nullptr){
+        return;
+      }
+      if(x < t->element){
+        remove(x, t->left);
+      }
+      else if(t->element < x) {
+        remove(x, t->right);
+      }
+      else{
+        if(t->right !=nullptr){
+          cout<<"2";
+          t->element=findMin(t->right)->element;
+          remove(t->element, t->right);
+
+
+        }
+        else{
+          BinaryNode* oldNode = t;
+         if (t->left!=nullptr){
+          cout<<"3";
+          t=t->left;
+
+        }
+        else{
+          t=nullptr;
+          cout<<"FUUUUCK";
+        }
+        delete oldNode;
+      }
+      }
+    }
+    void printInternal(BinaryNode* t, int offset) const
+    {
+        for (int i = 1; i <= offset; i++)
+            cout << "..";
+        if (t == nullptr)
+        {
+            cout << "#" << endl;
+            return;
+        }
+        cout << t->element << endl;
+        printInternal(t->left, offset + 1);
+        printInternal(t->right, offset + 1);
+        return;
+    }
+
+    // LAB 10: COMPLETE; to print in-line all elements in BST rooted in t;
+
+    void printElements(BinaryNode* t) const
+    {
+        if (t != 0)
+        {
+
+            if(t->left  !=nullptr)
+              printElements(t->left);
+
+            cout<<t->element<<endl;
+
+            if(t->right!=nullptr)
+              printElements(t->right);
+
+
+
+        }
+    }
+
+
+    // LAB 10: COMPLETE; returns the number of elements  stored in BST
+    //         rooted in t;
+
+    int size(BinaryNode* t) const
+    {
+        int yeet=0;
+        if(t->left!=nullptr){
+          yeet +=size(t->left);
+        }
+        if(t->right!=nullptr){
+          yeet +=size(t->right);
+        }
+        yeet+=1;
+        return yeet;
+    }
+
+};
+
+
+
+
+
+#endif
---
+++ lab10/Lab9Main.cpp
@@ -0,0 +1,58 @@
+// Lab10Main.cpp
+// KV for CSE 2020 in Spring 2024
+
+#include <iostream>
+#include "BinarySearchTreeLab9.h"  // completed version
+#include "Random.h"
+using namespace std;
+
+int main()
+{
+	rand_seed();
+	BinarySearchTree<int> mybst;
+	int how_many;
+	cout << "How many random values? ";
+	cin >> how_many;
+	cout << endl << endl;
+	std::vector<int> v;
+	int temp;
+	int temp2;
+
+	for (int i = 1; i <= how_many; i++)
+	{
+		temp = rand_int(1, 250);
+		mybst.insert(temp);
+		v.push_back(temp);
+	}
+	cout << endl << how_many << " values entered" << endl << endl;
+
+	cout << "Here are the current values: " << endl;
+	mybst.printElements();
+	cout << endl << endl;
+
+	cout << "Here is the structure of the BST: " << endl;
+	mybst.printInternal();
+	cout << endl << endl;
+
+	cout << "Number of values contained in BST: " << mybst.size();
+	cout << endl << endl;
+
+	cout <<"Now removing half of BST: ";
+	cout << endl << endl;
+	temp = v.size()/2;
+	for(int i=0; i<temp; i++){
+		temp2 = rand_int(0, v.size());
+		cout <<"Now removing " << v[temp2] <<endl;
+		mybst.remove(v[temp2]);
+		v.erase(v.begin()+temp2);
+	}
+	cout << endl << endl;
+	cout << "Here is the new structure of the BST: " << endl;
+	mybst.printInternal();
+	cout << endl << endl;
+
+	cout << "Number of values contained in BST: " << mybst.size();
+	cout << endl << endl;
+
+	return 0;
+}
