07695b5dd1d3e558f87698f8d6f79ee986a2a8bc
008320644 <008320644@csusb.edu@jb358-7.csusb.edu>
2024-04-24 13:06:15 -0700

    Fully functioning and submitted

--- lab13/Lab13Main.cpp
+++ lab13/Lab13Main.cpp
@@ -8,15 +8,29 @@ int main()
   rand_seed();
   Vector<int> v;
   int how_many;
-  cout << "How many random values? ";
+  cout << "How many random values for insertion sort? ";
   cin >> how_many;
-  cout << endl << endl;
+  cout << endl;
   int temp;
-  int temp2;
+  for (int i = 1; i <= how_many; i++){
+    temp = rand_int(1, 250);
+    v.push_back(temp);
+  }
+  for (int i=0; i< v.size(); i++){
+      cout<< v[i] << " ";
+    }
+    cout<<endl;
+    cout<< endl << "sorted into: "<< endl;
+  insertionSort(v);
   for (int i=0; i< v.size(); i++){
     cout<< v[i] << " ";
   }
+  cout<<endl;
+
+  cout << "How many random values for selection sort? ";
+  cin >> how_many;
   cout << endl;
+  v.clear();
   for (int i = 1; i <= how_many; i++)
   {
     temp = rand_int(1, 250);
@@ -25,5 +39,12 @@ int main()
   for (int i=0; i< v.size(); i++){
     cout<< v[i] << " ";
   }
+  cout<<endl;
+  cout<< endl << "sorted into: "<< endl;
+  selectionSort(v);
+  for (int i=0; i< v.size(); i++){
+    cout<< v[i] << " ";
+  }
+  cout<<endl;
   return 0;
 }
--- lab13/Sorting.h
+++ lab13/Sorting.h
@@ -6,16 +6,46 @@ template <typename T>
 void insertionSort(Vector<T>& a){
   T temp;
   int j;
-  for(int p=1; p<a.size; p++){
+  for(int p=1; p<a.size(); p++){
     j=p;
     temp=a[j];
-    for(; (j>0)&&(a[j-1]>a[j]); j--){
+    while((j>0)&&(a[j-1]>temp)){
       a[j]=a[j-1];
+      j--;
     }
     a[j]=temp;
   }
 }
+template <typename T>
 void selectionSort(Vector<T>& a){
-
+  T temp;
+  int tempint;
+  for(int i= a.size()-1; i>0; i--){
+    temp = a[0];
+    tempint =0;
+    for(int j=1; j<i; j++){
+      if(temp<a[j]){
+        temp =a[j];
+        tempint =j;
+      }
+    }
+    if(temp>a[i]){
+      a[tempint]=a[i];
+      a[i]=temp;
+    }
+  }
 }
 #endif
+
+
+
+// p=2
+// j=1
+// temp=2
+//      4| 5| 2| 3
+//      4| 5| 2| 3
+//      4| 5| 5| 3
+//
+//
+//
+//
