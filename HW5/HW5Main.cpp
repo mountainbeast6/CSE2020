d319694a5fd100dd4fcb05bbb1d99dae6fb3b73a
008320644 <008320644@csusb.edu@jb358-7.csusb.edu>
2024-04-29 13:52:58 -0700

    Fixed Crashing Problem, added text.

--- HW5/HW5Main.cpp
+++ HW5/HW5Main.cpp
@@ -12,19 +12,33 @@ int main(){
   string name;
   string temps;
   Pair<double, double> p1;
-  string lon;
-  string lat;
+  double lon;
+  double lat;
   int temp;
-  while(file.good()){
-    getline(file, line);
-    temp = line.find_first_of(" ");
+  bool loop = true;
+  string enetered1;
+  string enetered2;
+  for(getline(file, line);file.good();getline(file, line)){
+    temp = line.find_first_of(" ")+1;
     name = line.substr(0,temp);
     temps = line.substr(temp);
-    lon=temps.substr(0, temps.find_first_of(" ")-1);
-    lat=temps.substr(temps.find_first_of(" "));
-    // p1=(lon, lat);
-    // a[name]=p1;
-    cout<< name << endl<< lon<< " "<< lat<< endl;
+    lon=stod(temps.substr(0, temps.find_first_of(" ")));
+    lat=stod(temps.substr(temps.find_first_of(" ")));
+    p1.first=lon;
+    p1.second=lat;
+    a[name]=p1;
+    cout<< name << endl;
   }
   file.close();
+  while(loop){
+    cout<<"Hi there! Please Choose One of the Listed World Capitals! "<< endl;
+    cin>>enetered1;
+    cout<<"Alright now pick another! "<< endl;
+    cin>> entered2;
+
+  }
+  return 0;
+}
+double Haversine(double lon1, double lon2, double lat1, double lat2){
+  return 0.0;
 }
